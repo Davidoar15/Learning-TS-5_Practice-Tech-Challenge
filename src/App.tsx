8007e09cb6c436ace13d0ea16bf42c31e0f4b030
Davidoar15 <davidoar15@gmail.com>
2023-10-24 13:16:21 -0300

    initial_HTML_&_CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <main>
+        <aside>
+          <h1>Tech Challenge (React + TypeScript)</h1>
+          <h2>Add and Delete elements from a List</h2>
+          
+          <form>
+            <label>
+              Insert Element:
+              <input
+                required
+                type='text'
+                name='item' 
+                placeholder='Write...'/>
+            </label>
+            <button>
+              Add Element
+            </button>
+          </form>
+        </aside>
+
+        <section>
+          <h2>List</h2>
+          <ul>
+            <li>Element 1</li>
+            <li>Element 2</li>
+            <li>Element 3</li>
+            <li>Element 4</li>
+          </ul>
+        </section>
+      </main>
   )
 }
 

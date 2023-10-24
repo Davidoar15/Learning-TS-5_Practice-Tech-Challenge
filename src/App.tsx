94fe0bf277be8e62f4173d2863bd1b811b51b8af
Davidoar15 <davidoar15@gmail.com>
2023-10-24 13:59:38 -0300

    add_new_Item_from_Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
+import React, { useState } from 'react';
 import './App.css'
 
+interface Item {
+  id: `${string}-${string}-${string}-${string}-${string}`,// or String,
+  text: String,
+  timestamp: number,
+}
+
+const INITIAL_ITEMS: Item[] = [
+  {
+    id: crypto.randomUUID(),
+    text: "Element 1",
+    timestamp: Date.now(),
+  },
+  {
+    id: crypto.randomUUID(),
+    text: "Element 2",
+    timestamp: Date.now(),
+  },
+];
+
 function App() {
+
+  const [items, setItems] = useState(INITIAL_ITEMS);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { elements } = e.currentTarget;
+
+    const input = elements.namedItem('item');
+    const isInput = input instanceof HTMLInputElement;
+    if (!isInput || input == null) return;
+
+    const newItem: Item = {
+      id: crypto.randomUUID(),
+      text: input.value,
+      timestamp: Date.now()
+    }
+
+    setItems((prevItems) => {
+      return [...prevItems, newItem];
+    });
+
+    input.value = "";
+  };
 
   return (
       <main>
         <aside>
           <h1>Tech Challenge (React + TypeScript)</h1>
           <h2>Add and Delete elements from a List</h2>
-          
-          <form>
+
+          <form onSubmit={handleSubmit}>
             <label>
               Insert Element:
               <input
@@ -26,10 +70,15 @@ function App() {
         <section>
           <h2>List</h2>
           <ul>
-            <li>Element 1</li>
-            <li>Element 2</li>
-            <li>Element 3</li>
-            <li>Element 4</li>
+            {
+              items.map(item => {
+                return (
+                  <li key={item.id}>
+                    {item.text}
+                  </li>
+                )
+              })  
+            }
           </ul>
         </section>
       </main>
